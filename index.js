76e31f8e602af6bb2eb78bc318793bfe016adc20
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2026-01-02 10:34:28 +0530

    mongodb integrated

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("./app")
+const connectDB = require("./config/db")
+const User = require("./models/user_model")
+const PORT = 3000;
+
+app.get("/" , (req , res)=>{
+    res.send("Server is live now")
+})
+
+connectDB("mongodb://127.0.0.1:27017/todo").then(()=>{
+    console.log(`connected to db`);
+});
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running at the ${PORT}`)
+})
